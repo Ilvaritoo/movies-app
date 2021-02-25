bbc8bec262e7a7d2816f91accea91651d7b2d3ff
Ilvars Grimza <ilvars.grimza@gmail.com>
2021-02-25 17:01:01 +0200

    genres btn works for mobile

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -6,6 +6,7 @@ import './row.scss';
 const Row = ({test}) => {
 
     const [movie, setMovie] = useState([]);
+    const [isBtnVisible, setIsBtnVisible] = useState(false)
 
     useEffect(()=> {
         axios.get(test)
@@ -13,17 +14,26 @@ const Row = ({test}) => {
         .catch(error => console.log(error))
     }, [test]);
 
+    // CHANGE BUTTON VISIBILITY
+    const openGenres = () => {
+        setIsBtnVisible(isBtnVisible => !isBtnVisible)
+    };
 
+    console.log(isBtnVisible);
     return (
         <div className='row'>
-            <div className="genres">
-                <button className='genres__btn'>Popular</button>
-                <button className='genres__btn'>Action</button>
-                <button className='genres__btn'>Comedy</button>
-                <button className='genres__btn'>New</button>
-                <button className='genres__btn'>Cartoon</button>
-                <button className='genres__btn'>Scary</button>
-                <button className='genres__btn'>Most Rated</button>
+            <div className="row__genres__switch" onClick={openGenres}>
+                <h3>{isBtnVisible ? 'Close' : 'Genres'}</h3>
+            </div>
+            {/* <div className="row__genres"> */}
+            <div className={`row__genres ${isBtnVisible ? 'row__genres__visible' : ''}`}>
+                <button className='row__genres__btn'>Popular</button>
+                <button className='row__genres__btn'>Action</button>
+                <button className='row__genres__btn'>Comedy</button>
+                <button className='row__genres__btn'>New</button>
+                <button className='row__genres__btn'>Cartoon</button>
+                <button className='row__genres__btn'>Scary</button>
+                <button className='row__genres__btn'>Most Rated</button>
             </div>
             <div className="row__items">
                 {
